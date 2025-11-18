7ce92eb42ec7689fe45d956bedd9ba5ec1923e3a
[dyad] <vincentdaguplo9@gmail.com>
2025-11-18 17:44:26 +0800

    [dyad] Updated the "Made with" text to "Made by Zenin Clan BSCS-1C". - wrote 1 file(s)

--- src/components/made-with-zenin-clan.tsx
+++ src/components/made-with-zenin-clan.tsx
@@ -2,7 +2,7 @@ export const MadeWithZeninClan = () => {
   return (
     <div className="p-4 text-center">
       <span className="text-sm text-gray-500 dark:text-gray-400">
-        Made with Zenin Clan
+        Made by Zenin Clan BSCS-1C
       </span>
     </div>
   );
